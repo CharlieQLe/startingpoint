3f8a5d0f3cc58d99a6d1360bbdeb2423fdcabc6c
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-09-20 16:49:03 -0400

    Update to Fedora 39

--- config/scripts/hyprland-xwayland.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-wget "https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-${OS_VERSION}/solopasha-hyprland-fedora-${OS_VERSION}.repo" -P "/etc/yum.repos.d/"
-
-rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:hyprland'
